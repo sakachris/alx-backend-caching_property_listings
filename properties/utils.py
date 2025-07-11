0f3b30708fae6fe4412cdfbe1b0fb78d87ff4992
sakachris <sakachris90@gmail.com>
2025-07-11 20:31:07 +0300

    cache metrics

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,7 @@
 from django.core.cache import cache
 from .models import Property
+import logging
+from django_redis import get_redis_connection
 
 def get_all_properties():
     properties = cache.get('all_properties')
@@ -9,3 +11,32 @@ def get_all_properties():
         ))
         cache.set('all_properties', properties, timeout=3600)  # Cache for 1 hour
     return properties
+
+
+logger = logging.getLogger(__name__)
+
+def get_redis_cache_metrics():
+    try:
+        redis_conn = get_redis_connection("default")
+        info = redis_conn.info()
+
+        hits = info.get("keyspace_hits", 0)
+        misses = info.get("keyspace_misses", 0)
+
+        total = hits + misses
+        hit_ratio = hits / total if total > 0 else None
+
+        metrics = {
+            "keyspace_hits": hits,
+            "keyspace_misses": misses,
+            "hit_ratio": hit_ratio
+        }
+
+        logger.info(f"Redis Cache Metrics: {metrics}")
+        return metrics
+
+    except Exception as e:
+        logger.error(f"Failed to fetch Redis metrics: {e}")
+        return {
+            "error": str(e)
+        }
