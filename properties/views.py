9574c00a860345d9f878676adc3584f6c9728769
sakachris <sakachris90@gmail.com>
2025-07-11 20:17:13 +0300

    adding caching 15 mins 2

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+# properties/views.py
+
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
-@cache_page(60 * 15)  # Cache the response for 15 minutes
+@cache_page(60 * 15)  # Cache response in Redis for 15 minutes
 def property_list(request):
-    properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'location', 'created_at'
+    )
+    return JsonResponse({'data': list(properties)})
