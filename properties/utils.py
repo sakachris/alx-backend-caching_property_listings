d45384d5bd5d675b3ce235896735bbd70f9e8080
sakachris <sakachris90@gmail.com>
2025-07-11 20:23:08 +0300

    all properties

---
+++ properties/utils.py
@@ -0,0 +1,11 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = list(Property.objects.all().values(
+            'id', 'title', 'description', 'price', 'location', 'created_at'
+        ))
+        cache.set('all_properties', properties, timeout=3600)  # Cache for 1 hour
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -3,10 +3,16 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 
-@cache_page(60 * 15)  # Cache response in Redis for 15 minutes
+# @cache_page(60 * 15)  # Cache response in Redis for 15 minutes
+# def property_list(request):
+#     properties = Property.objects.all().values(
+#         'id', 'title', 'description', 'price', 'location', 'created_at'
+#     )
+#     return JsonResponse({'data': list(properties)})
+
+
 def property_list(request):
-    properties = Property.objects.all().values(
-        'id', 'title', 'description', 'price', 'location', 'created_at'
-    )
-    return JsonResponse({'data': list(properties)})
+    properties = get_all_properties()
+    return JsonResponse({'data': properties})
