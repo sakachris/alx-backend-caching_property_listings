0708a59f612a774db89a20283dd06de63ee270ad
sakachris <sakachris90@gmail.com>
2025-07-11 20:26:50 +0300

    cache invalidation

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,9 @@
+# properties/apps.py
 from django.apps import AppConfig
-
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        import properties.signals # Ensure signals are imported when the app is ready
