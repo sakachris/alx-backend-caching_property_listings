9702dc18c4f8a09d5ebd03975b8c11c20b208524
sakachris <sakachris90@gmail.com>
2025-07-11 20:14:07 +0300

    adding caching 15 mins

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)  # Cache the response for 15 minutes
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, 'properties/property_list.html', {'properties': properties})
