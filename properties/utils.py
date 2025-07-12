258ebfca9b19aa42a6d2bebed7886c49a96b3d90
sakachris <sakachris90@gmail.com>
2025-07-12 15:27:24 +0300

    utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -1,7 +1,54 @@
+# properties/utils.py
+# from django.core.cache import cache
+# from .models import Property
+# import logging
+# from django_redis import get_redis_connection
+
+# def get_all_properties():
+#     properties = cache.get('all_properties')
+#     if properties is None:
+#         properties = list(Property.objects.all().values(
+#             'id', 'title', 'description', 'price', 'location', 'created_at'
+#         ))
+#         cache.set('all_properties', properties, timeout=3600)  # Cache for 1 hour
+#     return properties
+
+
+# logger = logging.getLogger(__name__)
+
+# def get_redis_cache_metrics():
+#     try:
+#         redis_conn = get_redis_connection("default")
+#         info = redis_conn.info()
+
+#         hits = info.get("keyspace_hits", 0)
+#         misses = info.get("keyspace_misses", 0)
+
+#         total = hits + misses
+#         hit_ratio = hits / total if total > 0 else None
+
+#         metrics = {
+#             "keyspace_hits": hits,
+#             "keyspace_misses": misses,
+#             "hit_ratio": hit_ratio
+#         }
+
+#         logger.info(f"Redis Cache Metrics: {metrics}")
+#         return metrics
+
+#     except Exception as e:
+#         logger.error(f"Failed to fetch Redis metrics: {e}")
+#         return {
+#             "error": str(e)
+#         }
+
+
+import logging
 from django.core.cache import cache
 from .models import Property
-import logging
 from django_redis import get_redis_connection
+
+logger = logging.getLogger(__name__)
 
 def get_all_properties():
     properties = cache.get('all_properties')
@@ -12,9 +59,6 @@ def get_all_properties():
         cache.set('all_properties', properties, timeout=3600)  # Cache for 1 hour
     return properties
 
-
-logger = logging.getLogger(__name__)
-
 def get_redis_cache_metrics():
     try:
         redis_conn = get_redis_connection("default")
@@ -23,8 +67,8 @@ def get_redis_cache_metrics():
         hits = info.get("keyspace_hits", 0)
         misses = info.get("keyspace_misses", 0)
 
-        total = hits + misses
-        hit_ratio = hits / total if total > 0 else None
+        total_requests = hits + misses
+        hit_ratio = hits / total_requests if total_requests > 0 else 0
 
         metrics = {
             "keyspace_hits": hits,
